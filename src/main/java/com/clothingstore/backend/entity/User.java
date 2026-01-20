87a8a9f661cc75574a9baf437434aeeb16d48b0c
Nam Huynh Quoc <nam.huynh@spiraledge.com>
2026-01-20 15:10:16 +0700

    add user table

---
+++ src/main/java/com/clothingstore/backend/entity/User.java
@@ -0,0 +1,51 @@
+package com.clothingstore.backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Phone
+    @Column(unique = true, nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "is_block", nullable = false)
+    private Boolean isBlock = false;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AuthProvider provider = AuthProvider.LOCAL;
+
+    @Column(name = "provider_id")
+    private String providerId;
+}
